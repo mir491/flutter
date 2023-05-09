a8d9c1bda79ec3a0345be729718e0c7645c74cf1
mir umar <mir@coadjute.com>
2023-05-09 17:47:03 +0530

    my first program

---
+++ lib/learn1.dart
@@ -0,0 +1,23 @@
+main(){
+
+  print("alex");
+
+  /*String name1="jenny";
+  String name2="jolly"
+  print(name1+name2);*/
+/*int age=7;
+print(age);*/
+/*
+double age =10.10;
+print(age);
+bool time=true;
+print (time);
+List arr=[1,"linux","cotlin","sak"];
+print(arr[2]);
+*/
+Map arr={'name':'ali','city':'newzy'};
+print(arr['city']);
+
+
+
+}
